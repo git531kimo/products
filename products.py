9c8d00b87a1301dfeef89345a40c2c1de0d0c36b
Frank Shiau <git531kimo@yahoo.com.tw>
2020-07-29 17:05:48 +0800

    add header

--- products.py
+++ products.py
@@ -12,3 +12,8 @@ print(products)
 
 for p in products: # 用for loop來印出商品跟價格(大清單products中的小清單)
 	print(p[0], '的價格是', p[1])
+
+with open('products.csv', 'w', encoding='utf-8') as f: # 原本是.txt，改成csv是電腦常用檔案且可用excel打開
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n')
